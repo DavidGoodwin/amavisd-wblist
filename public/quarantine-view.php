5330f48f82eb1dc3e58386730ac058681fc8ef8a
David Goodwin <david@palepurple.co.uk>
2018-04-27 07:02:30 +0100

    improve quarantine view

--- public/quarantine-view.php
+++ public/quarantine-view.php
@@ -2,7 +2,7 @@
 
 require_once('common.php');
 
-if(empty($_GET['mail_id'])) {
+if (empty($_GET['mail_id'])) {
     \AmavisWblist\Flash::addError('no mail_id specified');
     header("Location: index.php");
     exit(0);
@@ -16,19 +16,34 @@ $template = new \AmavisWblist\Template();
 
 $mail = $database->queryOne('SELECT * FROM quarantine WHERE mail_id = ?', [$mail_id]);
 
-if(empty($mail)) {
+if (empty($mail)) {
     \AmavisWblist\Flash::addError("Mail not found");
     header("Location: amavis-recent-mail.php");
     exit(0);
 }
 
-if(is_resource($mail['mail_text'])) {
+if (is_resource($mail['mail_text'])) {
     $mail['mail_text'] = stream_get_contents($mail['mail_text']);
 
 }
 
-$template->setTitle("Quarantine View");
+$template->assign('mail_raw', $mail['mail_text']);
 
-$template->assign('mail', $mail['mail_text']);
+$pmail = new PhpMimeMailParser\Parser();
+$pmail->setText($mail['mail_text']);
+
+$text = $pmail->getMessageBody('text');
+$html = $pmail->getMessageBody('html');
+
+$template->assign('mail_plain', $text);
+$template->assign('mail_html_safe', strip_tags($html)); // don't trust html?
+
+$attachments = [];
+foreach ($pmail->getAttachments() as $attachment) {
+    $attachments[] = ['filename' => $attachment->getFilename(), 'content_type' => $attachment->getContentType()];
+}
+$template->assign('attachments', $attachments);
+
+$template->setTitle("Quarantine View");
 
 $template->display('quarantine-view.tpl');
--- templates/quarantine-view.tpl
+++ templates/quarantine-view.tpl
@@ -1,5 +1,24 @@
 <h2>Quarantined Email</h2>
 
+<h3>Attachments</h3>
+<ul>
+    {foreach from=$attachments item=attachment}
+        <li>{$attachment.filename|escape:"htmlall"} - {$attachment.content_type}</li>
+    {/foreach}
+</ul>
+
+
+<h3>Plain Text Content</h3>
 <pre>
-{$mail|escape:"htmlall"}
+    {$mail_plain|escape:"htmlall"}
+</pre>
+
+<h3>HTML Content</h3>
+<pre>
+    {$mail_html_safe|escape:"htmlall"}
+</pre>
+
+<h3>RAW BODY</h3>
+<pre>
+{$mail_raw|escape:"htmlall"}
 </pre>
